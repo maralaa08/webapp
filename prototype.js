53111904c8f2c3164042e330de628019d239d9a3
maralaa08 <Mamaralmaa66@gmail.com>
2024-05-13 13:43:34 +0800

    js

---
+++ prototype.js
@@ -0,0 +1,11 @@
+class prototype extends HTMLElement{
+    constructor (){
+        super(); 
+    }
+customElements.define([
+    'require',
+    'dependency'
+], function(require, factory) {
+    'use strict';
+    
+});
